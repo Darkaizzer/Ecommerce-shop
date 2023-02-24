2a10b9b0f0e31bb46758a6243388ec7be5030233
Darkaizer <dx_efron95@mail.ru>
2023-02-24 10:43:05 +0600

    add to adding a shopping cart, deleting an item

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React, { useContext } from 'react';
+//sidebarcontext 
 import { SidebarContext } from '../contexts/SidebarContext';
+//import ucons 
+import {BsBag} from "react-icons/bs"
 
 const Header = () => {
   const{isOpen, setIsOpen} = useContext(SidebarContext)
-  return <div>
-    <div>Header</div>
-    <div onClick={()=> setIsOpen(!isOpen)}>open/close sidebar</div>
-  </div>;
+  return (
+    <header className=' bg-pink-200'>
+      <div>Header</div>
+      <div onClick={() => setIsOpen(!isOpen)} className="cursor-pointer flex relative">
+        <BsBag className='text-2xl' />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
